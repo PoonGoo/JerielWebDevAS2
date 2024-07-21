b67759ae5f52745fd1cbdfbc50478425c2adf930
PoonGoo <jeriel.lim.sy@gmail.com>
2024-07-21 19:01:53 +0800

    did band page
    added a music player and imgs and audio

--- script.js
+++ script.js
@@ -150,6 +150,133 @@ document.addEventListener("DOMContentLoaded", function() {
     
      //-------------------------GALLERY PAGE END ------------------
 
+    //Music player 
+    const playPause = document.querySelector("#play-stop");
+    const backward = document.querySelector("#backward");
+    const forward = document.querySelector("#forward");
+
+    // record player animation
+    const circleBig = document.querySelector("#circle-bg");
+    const circleSm = document.querySelector("#circle-sm");
+
+    // playing song
+    const songName = document.querySelector("#song-name");
+    const audio = document.querySelector("#audio");
+    const coverArt = document.querySelector("#cover");
+    const musicbox = document.querySelector("#musicbox");
+
+    // control button images
+    let playImg = "images/play.svg";
+    let pauseImg = "images/pause.svg";
+
+    // default controls
+    playPause.src = playImg;
+    let isPlaying = true;
+
+    const songList = [
+        {
+            name: "Under The Bridge (Red Hot Chilli Pepepers)",
+            source: "audios/under-the-bridge.mp3    ",
+            cover: "images/suck-my-kiss.jpg"
+        },
+        {
+            name: "Everlong (Foo Fighters)",
+            source: "audios/everlong.mp3",
+            cover: "images/everlong.jpg"
+        },
+        {
+            name: "High and Dry (Radiohead)",
+            source: "audios/high-and-dry.mp3",
+            cover: "images/the-bends.jfif"
+        },
+        {
+            name: "Don't Look back In Anger",
+            source: "audios/dont-look.mp3",
+            cover: "images/whats-the-story.jfif"
+        },
+        {
+            name: "Basket Case",
+            source: "audios/basket-case.mp3",
+            cover: "images/basket-case.jpg"
+        }
+    ];
+    // helper function
+    function createEle(ele) {
+        return document.createElement(ele);
+    }
+    function append(parent, child) {
+        return parent.append(child);
+    }
+    // creating track list
+    const ul = createEle('ul')
+    function createPlayList() {
+        songList.forEach((song) => {
+            let h3 = createEle('h3');
+            let li = createEle('li');
+
+            li.classList.add("track-item");
+            h3.innerText = song.name;
+            append(li,h3);
+            append(ul,li)
+        })
+        append(musicbox, ul);
+    }
+
+    let songIndex = 0;
+    // preloaded song
+    loadMusic(songList[songIndex]);
+
+
+    function loadMusic() {
+        coverArt.src = songList[songIndex].cover;
+        songName.innerText = songList[songIndex].name;
+        audio.src = songList[songIndex].source;
+    }
+
+    function playSong() {
+        playPause.src = pauseImg;
+        circleBig.classList.add("animate");
+        circleSm.classList.add("animate");
+
+        audio.play();
+    }
+
+    function pauseSong() {
+        playPause.src = playImg;
+        circleBig.classList.remove("animate");
+        circleSm.classList.remove("animate");
+
+        audio.pause();
+    }
+
+    function nextPlay() {
+        songIndex++;
+        if(songIndex > songList.length - 1) {
+            songIndex = 0;
+        }
+        loadMusic(songList[songIndex]);
+        playSong()
+    }
+
+    function backPlay() {
+        songIndex--;
+        if(songIndex < 0) {
+            songIndex = songList.length - 1;
+        }
+        loadMusic(songList[songIndex]);
+        playSong()
+    }
+    function playHandler() {
+        isPlaying = !isPlaying;
+        isPlaying ? pauseSong() : playSong();
+    }
+
+
+    // player event 
+    playPause.addEventListener("click", playHandler);
+    backward.addEventListener("click", backPlay);
+    forward.addEventListener("click", nextPlay);
+    createPlayList()
 
     // Memory Card Game
     const startButton = document.getElementById("start-button");
